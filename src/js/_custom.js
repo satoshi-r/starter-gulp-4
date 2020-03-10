b51afb952244db94b1bb32e0a978d63d9ed84159
satoshi-r <rgeargerge@yandex.ru>
2020-03-10 18:38:03 +0300

    stop anim on resize

--- src/js/_custom.js
+++ src/js/_custom.js
@@ -1,5 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-	// Custom JS
-
+	// Остановить анимацию во время изменения размера окна
+	let resizeTimer;
+	window.addEventListener("resize", () => {
+		document.body.classList.add("resize-animation-stopper");
+		clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(() => {
+			document.body.classList.remove("resize-animation-stopper");
+		}, 400);
+	});
 });
