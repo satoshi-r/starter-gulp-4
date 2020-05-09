4c6c5b8657fe73f838372c18090e870995ac99b6
satoshi-r <rgeargerge@yandex.ru>
2020-05-09 23:10:57 +0300

    fix styles

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('browser-sync', () => {
 
 // Custom Styles
 gulp.task('styles', () => {
-	return gulp.src('src/sass/**/*.sass')
+	return gulp.src(['src/sass/**/*.sass', 'src/scss/**/*.scss'])
 		.pipe(sass({
 			outputStyle: 'expanded',
 			includePaths: [__dirname + '/node_modules']
@@ -174,7 +174,7 @@ gulp.task('build', gulp.series('clean:build', gulp.parallel('css:build', 'js:bui
 
 // Watch
 gulp.task('watch', () => {
-	gulp.watch('src/sass/**/*.sass', gulp.parallel('styles'));
+	gulp.watch(['src/sass/**/*.sass', 'src/scss/**/*.scss'], gulp.parallel('styles'));
 	gulp.watch(['src/js/_custom.js', 'src/js/_libs.js'], gulp.parallel('scripts'));
 	gulp.watch('src/*.html', gulp.parallel('code'));
 	gulp.watch('src/img/_src/**/*', gulp.parallel('img'));
